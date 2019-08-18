4f876b0d579e9f8cb785c6af741f75aaf10fa42e
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2019-08-17 21:38:14 -0700

    Structure the project in MVVM design

--- app/src/main/java/c/bmartinez/dependencyinjectionwithkodein/UI/quotes/QuotesActivity.kt
+++ app/src/main/java/c/bmartinez/dependencyinjectionwithkodein/UI/quotes/QuotesActivity.kt
@@ -1,12 +1,13 @@
-package c.bmartinez.dependencyinjectionwithkodein
+package c.bmartinez.dependencyinjectionwithkodein.UI.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import c.bmartinez.dependencyinjectionwithkodein.R
 
 class QuotesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_quotes)
     }
 }
