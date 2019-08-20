154e2fe59769b8b95aead73f2c0f3dad73f9fbc7
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2019-08-19 23:23:50 -0700

    Setup the Quote data class and the data access objects

---
+++ app/src/main/java/c/bmartinez/dependencyinjectionwithkodein/Data/db/QuoteDao.kt
@@ -0,0 +1,10 @@
+package c.bmartinez.dependencyinjectionwithkodein.Data.db
+
+import androidx.lifecycle.LiveData
+import c.bmartinez.dependencyinjectionwithkodein.Data.model.Quote
+
+interface QuoteDao {
+    fun addQuote(quote: Quote)
+    fun getQuotes(): LiveData<List<Quote>>
+
+}
