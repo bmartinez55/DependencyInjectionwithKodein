8d7b8792770164eacbb240cc8737f8704e75cd0d
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2019-08-20 17:56:24 -0700

    Project tutorial completed: Dependency Injection using Kodein Library

--- app/src/main/java/c/bmartinez/dependencyinjectionwithkodein/Data/db/DatabaseFakeImpl.kt
+++ app/src/main/java/c/bmartinez/dependencyinjectionwithkodein/Data/db/DatabaseFakeImpl.kt
@@ -1,5 +1,7 @@
 package c.bmartinez.dependencyinjectionwithkodein.Data.db
 
+import c.bmartinez.dependencyinjectionwithkodein.Data.model.Quote
+
 class DatabaseFakeImpl: Database {
     override val quoteDao: QuoteDao = QuoteDaoFake()
 }
